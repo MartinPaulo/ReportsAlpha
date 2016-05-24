b54c888fffce01a15e0978fa69007c5405277182
Martin Paulo <martin.paulo@gmail.com>
2016-05-24 11:50:36 +1000

    Set colours for faculty

--- reports/static/reports/storage_quota.js
+++ reports/static/reports/storage_quota.js
@@ -1,9 +1,24 @@
 var report = report || {};
 
 report.d3 = {
-    render: function (csv_path) {
 
-        d3.json(csv_path, function (error, data) {
+
+    render: function (jsonPath) {
+
+        var schoolColors = {
+            'VCAMCM': '#1f77b4',
+            'VAS': '#ff7f0e',
+            'FoS': '#2ca02c',
+            'MDHS': '#d62728',
+            'MLS': '#9467bd',
+            'MSE': '#8c564b',
+            'MGSE': '#e377c2',
+            'FBE': '#7f7f7f',
+            'FoA': '#bcbd22',
+            'ABP': '#17becf'
+        };
+
+        d3.json(jsonPath, function (error, data) {
             var chart = nv.models.stackedAreaChart()
                 .margin({right: 100})
                 .x(function (d) {
@@ -15,8 +30,11 @@ report.d3 = {
                 .useInteractiveGuideline(true)    //Tooltips which show all data points. Very nice!
                 .rightAlignYAxis(true)      //Let's move the y-axis to the right side.
                 .showControls(true)       //Allow user to choose 'Stacked', 'Stream'
-                .clipEdge(true).yDomain([0,100])
-                .controlOptions(['Stacked', 'Stream']);
+                .clipEdge(true).yDomain([0, 100])
+                .controlOptions(['Stacked', 'Stream'])
+                .color(function (d) {
+                    return schoolColors.hasOwnProperty(d['key']) ? schoolColors[d['key']] : 'black';
+                });
 
             //Format x-axis labels with custom function.
             chart.xAxis
