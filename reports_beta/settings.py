763b011443c6d072bcd417ae3d1ad0b036442de1
Martin Paulo <martin.paulo@gmail.com>
2016-06-06 15:42:42 +1000

    Fix for version number not being shown correctly on production server.

--- reports_beta/settings.py
+++ reports_beta/settings.py
@@ -16,6 +16,10 @@ import logging
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SOURCE_REVISION_COMMANDS = [
+    'git --git-dir=%s/.git rev-parse --short HEAD' % BASE_DIR,  # for git
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
