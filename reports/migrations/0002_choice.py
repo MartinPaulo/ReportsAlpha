482c2bf70de071c3476806565db55d02cb560d76
Martin Paulo <martin.paulo@gmail.com>
2016-06-07 14:22:26 +1000

    Brought into line with database

--- reports/migrations/0002_choice.py
+++ reports/migrations/0002_choice.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Choice',
             fields=[
-                ('target_id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('choice_text', models.CharField(max_length=200)),
                 ('votes', models.IntegerField(default=0)),
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='reports.Report')),
