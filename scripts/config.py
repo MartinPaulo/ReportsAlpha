4c6dbc1eaf2c3a698b7a9f3517fb5e16a12d6d40
Martin Paulo <martin.paulo@gmail.com>
2016-08-19 16:37:47 +1000

    Now reads from two database sections in config file

--- scripts/config.py
+++ scripts/config.py
@@ -4,18 +4,28 @@ import os
 import sys
 from configparser import ConfigParser
 
+# default for local development
 reporting_db = {
     'user': 'root',
-    'passwd': 'B0nglyBeep',
+    'passwd': 'Password',
     'db': 'reporting',
+    'host': '192.168.33.1',
+    'port': 3306,
+}
+
+# default for local development
+production_db = {
+    'user': 'root',
+    'passwd': 'Password',
+    'db': 'dashboard',
     'host': '192.168.33.1',
     'port': 3306,
 }
 
 
 class Configuration(object):
-
     reporting_db = None
+    production_db = None
 
     @classmethod
     def load_from(cls, config_file):
@@ -25,18 +35,31 @@ class Configuration(object):
         logging.info("Loading configuration from %s", config_file)
         parser = ConfigParser()
         parser.read(config_file)
-        if not parser.has_section('reporting_db'):
-            logging.critical("No reporting database configuration in config")
-            sys.exit(1)
-        cls.reporting_db = {}
-        for (name, value) in parser.items('reporting_db'):
-            cls.reporting_db[name] = value
-        if reporting_db.keys() != cls.reporting_db.keys():
-            logging.critical("Reporting database mapping doesn't match")
-            sys.exit(1)
+        cls.read_db_section(parser, 'production_db', cls.production_db)
+        cls.read_db_section(parser, 'reporting_db', cls.reporting_db)
 
     @classmethod
-    def get_prod_db(cls):
+    def read_db_section(cls, parser, section_name, target):
+        if parser.has_section(section_name):
+            logging.info("Reading %s configuration", section_name)
+            target = {}
+            for (name, value) in parser.items(section_name):
+                target[name] = value
+            # we assume that bot the reporting and the production have the
+            # same number of values
+            if target.keys() != cls.reporting_db.keys():
+                logging.critical("Database %s mapping doesn't match",
+                                 section_name)
+                sys.exit(1)
+
+    @classmethod
+    def production_db(cls):
+        if not cls.production_db:
+            cls.production_db = production_db
+        return cls.production_db
+
+    @classmethod
+    def get_reporting_db(cls):
         if not cls.reporting_db:
             cls.reporting_db = reporting_db
         return cls.reporting_db
