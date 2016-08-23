0d34946910a8e78c13e98e7676a2880e535e1aad
Martin Paulo <martin.paulo@gmail.com>
2016-08-24 09:22:05 +1000

    Added the new settings to the default template.

--- reports_beta/local_settings_template.py
+++ reports_beta/local_settings_template.py
@@ -1,3 +1,23 @@
 # This file shows the settings that can be placed in local_settings.py
 # local_settings.py is a way for us to have machine specific information
 # if (any)
+
+
+# The reporting database against which the cloud reports are run.
+reporting_db = {
+    'user': 'root',
+    'passwd': 'Password',
+    'db': 'reporting',
+    'host': '192.168.33.1',
+    'port': 3306,
+}
+
+# The production databases, probably never going to be used in real life,
+# but useful during development.
+production_db = {
+    'user': 'root',
+    'passwd': 'Password',
+    'db': 'dashboard',
+    'host': '192.168.33.1',
+    'port': 3306,
+}
