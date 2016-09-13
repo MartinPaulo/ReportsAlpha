ad6d639f46fe97a82431359dd64dad4ec631e7a9
Martin Paulo <martin.paulo@gmail.com>
2016-09-13 11:36:59 +1000

    Added some extra logging

--- scripts/cloud/build_project_faculty.py
+++ scripts/cloud/build_project_faculty.py
@@ -13,19 +13,27 @@ if not ldap_connection.bind():
     sys.exit(1)
 
 
-def build_project_faculty(extract_db, load_db, start_day,
-                          end_day=date.today()):
-    logging.info("Building project faculty data from %s till %s ",
-                 start_day, end_day)
+def build_project_faculty(extract_db, load_db):
+    logging.info("Building project faculty data for all projects")
     result_set = extract_db.get_uom_project_contact_email()
+    faculty_members_found = 0
+    more_than_one_faculty = 0
+    projects_found = len(result_set)
     for row in result_set:
         contact_email = row["contact_email"]
         project_id = row["tenant_uuid"]
         project_name = row["tenant_name"]
         faculties = find_project_leader_faculty(contact_email)
         logging.info("Leader %s belongs to %s", contact_email, faculties)
+        if len(faculties) > 1:
+            more_than_one_faculty += 1
+        if faculties != {'Unknown'}:
+            faculty_members_found += 1
         load_db.save_faculty_data(faculties, contact_email, project_id,
                                   project_name)
+    logging.info("Of %s projects, %s were found, with %s having "
+                 "more than one faculty", projects_found,
+                 faculty_members_found, more_than_one_faculty)
 
 
 def find_project_leader_faculty(project_leader):
