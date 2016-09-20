f74c4905b7e794658cc20c07e1b2470266db2b30
Martin Paulo <martin.paulo@gmail.com>
2016-09-20 15:26:27 +1000

    Changed second log message

--- scripts/cloud/build_allocated.py
+++ scripts/cloud/build_allocated.py
@@ -8,7 +8,7 @@ def build_faculty_allocated(extract_db, load_db, start_day=None,
                             end_day=date.today()):
     if not start_day:
         start_day = load_db.get_faculty_allocated_last_run_date()
-    logging.info("Building allocated data from %s till %s",
+    logging.info("Building cloud allocated data from %s till %s",
                  start_day, end_day)
     for day_date in date_range(start_day, end_day):
         logging.info("Building cloud allocated data for %s", day_date)
