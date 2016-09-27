19ac77e8d87abf0129fa96cece54930297ad6aa3
Martin Paulo <martin.paulo@gmail.com>
2016-09-27 15:25:54 +1000

    Switched to using a regular assert

--- scripts/cloud/utility.py
+++ scripts/cloud/utility.py
@@ -9,7 +9,7 @@ def date_range(start_date, end_date):
     :param end_date: the day after the last date to be yielded.
     :return: yields the next date on
     """
-    if not start_date < end_date: raise AssertionError
+    assert start_date < end_date, "Start date must be before end date"
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
@@ -17,30 +17,30 @@ def date_range(start_date, end_date):
 def get_faculty(department_numbers):
     result = set()
     for departmentNumber in department_numbers:
-        depNo = int(departmentNumber[:3])
-        if 100 <= depNo < 250:
+        dep_no = int(departmentNumber[:3])
+        if 100 <= dep_no < 250:
             result.add('FoA')  # Faculty of Arts: ARTS in spreadsheet
-        elif 250 <= depNo < 300:
-            result.add(
-                'VAS')  # VAS? Veterinary and Agricultural Sciences: VET SC in spreadsheet
-        elif 300 <= depNo < 400:
+        elif 250 <= dep_no < 300:
+            # Veterinary and Agricultural Sciences: VET SC in spreadsheet
+            result.add('VAS')
+        elif 300 <= dep_no < 400:
             result.add('FBE')  # Faculty of Business and Economics
-        elif 400 <= depNo < 460:
+        elif 400 <= dep_no < 460:
             result.add('MSE')  # Melbourne School Of Engineering
-        elif 460 <= depNo < 500:
+        elif 460 <= dep_no < 500:
             result.add('MGSE')  # Melbourne Graduate School of Education
-        elif 500 <= depNo < 600:
+        elif 500 <= dep_no < 600:
             result.add('MDHS')  # Medicine, Dentistry and Health Science
-        elif 600 <= depNo < 700:
+        elif 600 <= dep_no < 700:
             result.add('FoS')  # FoS? Faculty of Science: SCI in spreadsheet
-        elif 700 <= depNo < 730:
+        elif 700 <= dep_no < 730:
             result.add('ABP')  # Architecture, Building and Planning
-        elif 730 <= depNo < 750:
+        elif 730 <= dep_no < 750:
             result.add('MLS')  # Melbourne Law School
-        elif 750 <= depNo < 780:
-            result.add(
-                'VCAMCM')  # Victorian College of the Arts and Melbourne Conservatorium of Music
-        elif 780 <= depNo < 900:
+        elif 750 <= dep_no < 780:
+            # Victorian College of the Arts & Melbourne Conservatorium of Music
+            result.add('VCAMCM')
+        elif 780 <= dep_no < 900:
             result.add('MDHS')  # Medicine, Dentistry and Health Science
             # result.add('Bio 21') # Bio 21 goes to MDHS
         else:
