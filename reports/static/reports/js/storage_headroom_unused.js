16f8a9e8c65468eda2c6f4defebbd69c41f5a735
Martin Paulo <martin.paulo@gmail.com>
2016-09-27 16:34:51 +1000

    Moved to using common storage graph

---
+++ reports/static/reports/js/storage_headroom_unused.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var report = report || {};
+
+report.d3 = function () {
+
+    utils.createDateButtons();
+
+    var render = function () {
+        var data_path = '/reports/actual/?from=' + utils.findFrom() +
+            '&model=StorageHeadroomUnused';
+                // set the download link on the page
+        d3.select('#a_data').attr('href', data_path);
+        utils.renderStorageChart(data_path, utils.STORAGE_PRODUCT_TYPES);
+    };
+    // listen for redraw events
+    d3.select('#chart svg')[0][0].addEventListener('redraw', function (e) {
+        render();
+    }, false);
+
+    return {
+        render: render
+    }
+}();
