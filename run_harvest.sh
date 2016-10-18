38822a64f65f625be5238ad4737369dc8ca0ca3c
Martin Paulo <martin.paulo@gmail.com>
2016-10-18 15:52:01 +1100

    Modified to select target virtualenv on production machine (a hack)

--- run_harvest.sh
+++ run_harvest.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+# activate the environment with the required python libraries
+ENV=reporting
+source $WORKON_HOME/$ENV/bin/activate
+
 # set stdin, stdout and stderr to be unbuffered.
 PYTHONUNBUFFERED=1
 export PYTHONUNBUFFERED
 
-# the directory this script is in
+# the directory this script is in becomes the pythonpath
 export PYTHONPATH=$(pwd)
 
-echo $PYTHONPATH
-
+# but we are going work in the home directory of the scripts themsel
 cd scripts
 python runner.py
