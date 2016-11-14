358c47f316dcd11ee5cee2c8d94e488bfb1d4e1d
Martin Paulo <martin.paulo@gmail.com>
2016-11-14 14:58:51 +1100

    Changed install.sh to workout its parent directory
    If it's going to work with cron it needs to either be run in a given directory
    or get there itself. This is my first stumbling effort to get it there itself.

--- run_harvest.sh
+++ run_harvest.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# a bash script to run the extract/transform/load scripts on the production
+# server.
+
 # activate the environment with the required python libraries
 ENV=reporting
 source $WORKON_HOME/$ENV/bin/activate
@@ -9,8 +12,8 @@ PYTHONUNBUFFERED=1
 export PYTHONUNBUFFERED
 
 # the directory this script is in becomes the pythonpath
-export PYTHONPATH=$(pwd)
+export PYTHONPATH=$( cd $(dirname $0) ; pwd -P )
 
-# but we are going work in the home directory of the scripts themsel
+# but we are going work in the home directory of the scripts themselves
 cd scripts
 python runner.py
