f2537c2c8ec6bd2f4a304a05f586a81407d5ff52
Martin Paulo <martin.paulo@gmail.com>
2016-11-16 11:26:17 +1100

    Updated to hardwire in the path to the virtual env directories
    When run under cron there is no knowledge of the users .bash settings.
    Hence they have to explicitly be set for running under cron.
    The downside is that this makes parts of this script machine specific.

--- run_harvest.sh
+++ run_harvest.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
 # a bash script to run the extract/transform/load scripts on the production
 # server.
 
 # activate the environment with the required python libraries
 ENV=reporting
-source $WORKON_HOME/$ENV/bin/activate
+# if there is no workon home ie: being run by cron, then hardwire the path :(
+WORKON_HOME=$WORKON_HOME || /home/rapporteur/Virtualenvs
+# shellcheck source=/dev/null
+source "$WORKON_HOME/$ENV/bin/activate"
 
 # set stdin, stdout and stderr to be unbuffered.
 PYTHONUNBUFFERED=1
 export PYTHONUNBUFFERED
 
 # the directory this script is in becomes the pythonpath
-export PYTHONPATH=$( cd $(dirname $0) ; pwd -P )
+PYTHONPATH=$( cd "$(dirname "$0")" ; pwd -P )
+export PYTHONPATH
 
 # but we are going work in the home directory of the scripts themselves
-cd ${PYTHONPATH}/scripts
+cd "${PYTHONPATH}/scripts"
 python runner.py
