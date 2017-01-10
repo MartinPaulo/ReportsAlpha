07d69a90b2fab7d8b2605069bd113f207004c54d
Martin Paulo <martin.paulo@gmail.com>
2017-01-10 16:29:13 +1100

    Added a generator method to return quarter start and end dates
    We need to know when the quarters are for the buyers report. This method
    generates the needed dates.

--- scripts/cloud/utility.py
+++ scripts/cloud/utility.py
@@ -1,22 +1,64 @@
-from datetime import timedelta
+from datetime import timedelta, date
+import calendar
 
 
 def date_range(start_date, end_date):
-    """
-    A generator that returns all dates from and including the start date
+    """ A generator that returns all dates from and including the start date
     to the day before the end date.
-    :param start_date: the first date to be yielded
-    :param end_date: the day after the last date to be yielded.
-    :return: yields the next date on
+
+    Args:
+        start_date (date): the first date to be yielded
+        end_date (date): the day after the last date to be yielded.
+
+    Yields:
+        date: The next date in the range of `start_date` to `end_date` - 1 day
     """
     assert start_date < end_date, "Start date must be before end date"
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
 
+def quarter_dates(to_date=None):
+    """
+    Args:
+        to_date (date): The upper date to generate to. If `None` then will
+                        default to date.today()
+
+    Yields:
+        quarter_start (date), quarter_end (date): The quarter start and end
+        dates from the start of NecTAR in 2012 till the last complete quarter
+        before the to_date
+        yielded in pairs
+
+    Examples:
+        >>> for q1, q2 in quarter_dates(date(2013, 2, 1)):
+        >>>     print('%s %s' % (q1, q2))
+        2012-01-01 2012-03-31
+        2012-04-01 2012-06-30
+        2012-07-01 2012-09-30
+        2012-10-01 2012-12-31
+    """
+    year = 2012
+    end_date = date.today() if to_date is None else to_date
+    while year <= end_date.year:
+        quarter_starts = [date(year, month, 1) for month in (1, 4, 7, 10)]
+        quarter_ends = [
+            date(year, month, calendar.monthrange(year, month)[1])
+            for month in (3, 6, 9, 12)]
+        quarters = list(zip(quarter_starts, quarter_ends))
+        year += 1
+        for quarter in quarters:
+            if quarter[1] < end_date:
+                yield quarter[0], quarter[1]
+
+
 def _append(result, new_item):
     """
-    Will append new_item to result if new_item not already in result
+    Will append `new_item` to `result` iff `new_item` not already in `result`
+
+    Args:
+        result (list): the list to append `new_item` to if not already there
+        new_item: the object to append if not already in `result`
     """
     if new_item not in result:
         result.append(new_item)
