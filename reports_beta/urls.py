aa74b268084916505f163a8bfa93a23718cfb733
Martin Paulo <martin.paulo@gmail.com>
2017-06-21 10:27:02 +1000

    Set the administration title and header to 'ResPlat Reporting'

--- reports_beta/urls.py
+++ reports_beta/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from .views import index
 
+admin.site.site_header = 'ResPlat Reporting: Administration'
+admin.site.site_title = 'ResPlat Reporting'
+
 urlpatterns = [
     url('^$', index, name='index'),
     url(r'^uom_admin/doc/', include('django.contrib.admindocs.urls')),
