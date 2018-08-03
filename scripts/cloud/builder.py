0e138c691726fb2ababc386a2ee5618d1a6b3c53
Martin Paulo <martin.paulo@gmail.com>
2018-08-03 11:36:11 +1000

    Changed to use the new Faculties and LDAP classes in the utilities package

--- scripts/cloud/builder.py
+++ scripts/cloud/builder.py
@@ -8,9 +8,8 @@ from django.core.mail import mail_admins
 from django.utils.dateparse import parse_date
 
 from reports.models import CloudPrivateCell, CloudQuarterly, \
-    CloudQuarterlyUsage, CloudProjectFaculty
-from scripts.cloud.build_project_faculty import get_faculties_for
-from scripts.cloud.utility import date_range, Faculties, quarter_dates
+    CloudQuarterlyUsage, CloudProjectFaculty, CloudQuarterlyCoreHours
+from scripts.cloud.utility import date_range, Faculties, quarter_dates, LDAP
 
 
 def _get_non_null(value, default):
@@ -149,7 +148,7 @@ def test_db(extract_db, load_db, start_day=None, end_day=date.today()):
     # find out when we last took readings from the instances table
     if not start_day:
         start_day = load_db.get_used_last_run_date()
-    logging.info("Checking if entries since last run date %s ", start_day)
+    logging.info("Checking if entries since start day %s ", start_day)
     count = extract_db.count_instances_since(start_day)
     if count <= 0:
         warning_message = "No instances have been launched since %s"
@@ -161,10 +160,16 @@ def test_db(extract_db, load_db, start_day=None, end_day=date.today()):
     found_cell_names = extract_db.get_cell_names()
     differences = settings.CELL_NAMES.symmetric_difference(found_cell_names)
     if len(differences):
+        # The cell names change order between runs...
+        # This message goes out every day until something is done about
+        # the change in cell names. This is too verbose. The change should
+        # somehow be remembered and a new message only sent if they change
+        # again
         warning_message = "NeCTAR cell names have been changed! "
         information = "Cell Names have changed. The differences are: %s"
         logging.warning(information, differences)
-        mail_admins(warning_message, information % differences)
+        mail_admins(warning_message, information % differences,
+                    fail_silently=True)
 
 
 def build_capacity(unknown_source, load_db, start_day=None,
@@ -208,139 +213,221 @@ def build_private_cell_data(extract_db, load_db, start_day=None,
 
 def build_buyers_committee(extract_db, load_db, **kwargs):
     logging.info("Building quarterly usage data")
-    usage_start_date = _last_record_date(CloudQuarterlyUsage,
-                                         'date',
-                                         'date',
+    usage_start_date = _last_record_date(CloudQuarterlyUsage, 'date', 'date',
                                          date(2011, 12, 31))
     quarterly_start_date = _last_record_date(CloudQuarterly, 'date', 'date',
                                              date(2011, 12, 31))
-    for start_date, end_date in quarter_dates():
+    cpu_hours_start_date = _last_record_date(CloudQuarterlyCoreHours, 'date',
+                                             'date', date(2011, 12, 31))
+    for start_date, end_date, _quarter in quarter_dates():
         if usage_start_date < start_date:
-            projects_active = extract_db.get_projects_active(start_date,
-                                                             end_date)
-            uom_approved_projects_active = extract_db.get_uom_projects_active(
-                start_date,
-                end_date)
-            uom_all_projects_active = extract_db.get_all_uom_projects_active(
-                start_date,
-                end_date
-            )
-            uom_participation = extract_db.get_projects_active_with_uom_participation(
-                start_date,
-                end_date)
-            all_users_active = extract_db.get_admins_active(start_date,
-                                                            end_date)
-            uom_users_active = extract_db.get_uom_users_active(start_date,
-                                                               end_date)
-
-            CloudQuarterlyUsage.objects.update_or_create(
-                date=end_date.strftime("%Y-%m-%d"),
-                projects_active=projects_active,
-                uom_projects_active=uom_approved_projects_active,
-                all_uom_projects_active=uom_all_projects_active,
-                uom_participation=uom_participation,
-                all_users_active=all_users_active,
-                uom_users_active=uom_users_active
-            )
+            _build_activity_figures(extract_db, start_date, end_date)
         if quarterly_start_date < start_date:
-            totals = Faculties.get_new_totals()
-            result_set = extract_db.get_email_of_active_uom_users(start_date,
-                                                                  end_date)
-            for row in result_set:
-                faculties = get_faculties_for(row['email'])
-                # for faculty in faculties:
-                #     totals[faculty] += 1
-                totals[faculties[0]] += 1
-            CloudQuarterly.objects.update_or_create(
-                date=end_date.strftime("%Y-%m-%d"),
-                defaults={
-                    'foa': totals['FoA'],
-                    'vas': totals['VAS'],
-                    'fbe': totals['FBE'],
-                    'mse': totals['MSE'],
-                    'mgse': totals['MGSE'],
-                    'mdhs': totals['MDHS'],
-                    'fos': totals['FoS'],
-                    'abp': totals['ABP'],
-                    'mls': totals['MLS'],
-                    'vcamcm': totals['VCAMCM'],
-                    'services': totals['Other'],
-                    'unknown': totals['Unknown']
-                }
-            )
+            _build_users_faculties(extract_db, start_date, end_date)
+        if cpu_hours_start_date < start_date:
+            _build_cpu_hours(extract_db, start_date, end_date)
 
 
-def print_bc_percentages(extract_db, load_db, **kwargs):
+def _build_cpu_hours(extract_db, start_date, end_date):
+    logging.info('Building quarterly core hours')
+    core_hours = extract_db.get_core_hours(start_date, end_date)
+    vcpu_hours = Faculties.get_new_totals()
+    for core_hour in core_hours:
+        project_id = core_hour['project_id']
+        try:
+            project_faculty = CloudProjectFaculty.objects.get(
+                project_id=project_id)
+            project_faculty = project_faculty.allocated_faculty
+            vcpu_hours[Faculties(project_faculty)] += core_hour[
+                'vcpu_hours']
+        except CloudProjectFaculty.DoesNotExist:
+            logging.warning(
+                '%s with id %s does not have a faculty assigned' % (
+                    core_hour['project_name'], project_id))
+    CloudQuarterlyCoreHours.objects.update_or_create(
+        date=end_date.strftime("%Y-%m-%d"),
+        defaults={
+            'foa': vcpu_hours[Faculties.FOA],
+            'vas': vcpu_hours[Faculties.VAS],
+            'fbe': vcpu_hours[Faculties.FBE],
+            'mse': vcpu_hours[Faculties.MSE],
+            'mgse': vcpu_hours[Faculties.MGSE],
+            'mdhs': vcpu_hours[Faculties.MDHS],
+            'fos': vcpu_hours[Faculties.FOS],
+            'abp': vcpu_hours[Faculties.ABP],
+            'mls': vcpu_hours[Faculties.MLS],
+            'vcamcm': vcpu_hours[Faculties.VCAMCM],
+            'services': vcpu_hours[Faculties.OTHER],
+            'unknown': vcpu_hours[Faculties.UNKNOWN]
+        }
+    )
+
+
+def _build_users_faculties(extract_db, start_date, end_date):
+    user_totals = Faculties.get_new_totals()
+    active_uom_users = extract_db.get_email_of_active_uom_users(start_date,
+                                                                end_date)
+    ldap = LDAP()
+    for uom_user in active_uom_users:
+        faculties = ldap.find_faculty(uom_user['email'])
+        user_totals[faculties[0]] += 1
+    CloudQuarterly.objects.update_or_create(
+        date=end_date.strftime("%Y-%m-%d"),
+        defaults={
+            'foa': user_totals['FoA'],
+            'vas': user_totals['VAS'],
+            'fbe': user_totals['FBE'],
+            'mse': user_totals['MSE'],
+            'mgse': user_totals['MGSE'],
+            'mdhs': user_totals['MDHS'],
+            'fos': user_totals['FoS'],
+            'abp': user_totals['ABP'],
+            'mls': user_totals['MLS'],
+            'vcamcm': user_totals['VCAMCM'],
+            'services': user_totals['Other'],
+            'unknown': user_totals['Unknown']
+        }
+    )
+
+
+def _build_activity_figures(extract_db, start_date, end_date):
+    projects_active = extract_db.get_projects_active(start_date,
+                                                     end_date)
+    uom_approved_projects_active = extract_db.get_approved_uom_projects_active(
+        start_date,
+        end_date)
+    uom_all_projects_active = extract_db.get_all_uom_projects_active(
+        start_date,
+        end_date
+    )
+    uom_participation = extract_db.get_all_projects_active_with_uom_participation(
+        start_date,
+        end_date)
+    all_users_active = extract_db.get_admins_active(start_date,
+                                                    end_date)
+    uom_users_active = extract_db.get_uom_users_active(start_date,
+                                                       end_date)
+    CloudQuarterlyUsage.objects.update_or_create(
+        date=end_date.strftime("%Y-%m-%d"),
+        projects_active=projects_active,
+        uom_projects_active=uom_approved_projects_active,
+        all_uom_projects_active=uom_all_projects_active,
+        uom_participation=uom_participation,
+        all_users_active=all_users_active,
+        uom_users_active=uom_users_active
+    )
+
+def _get_quarter_abbreviation(quarter_end_date):
     """
-    Prints the contents of the cloud_quarterly table as percentages
+    By working out the quarter abbreviation from the date value here
+    we can see in the output that we are matching the date arguments
+    passed into the methods
+    :param quarter_end_date: a string in the format 'YYYY-MM-DD'.
+    E.g.: '2012-03-31'
+    :return: A string giving the quarter abbreviation. E.g.: '2012Q1'
     """
-    logging.info('Building quarterly usage percentages')
-    quarters = CloudQuarterly.objects.all().order_by('-date')[:5]
+    quarter_d = {3: 'Q1', 6: 'Q2', 9: 'Q3', 12: 'Q4'}
+    d = date(*[int(x) for x in quarter_end_date.split('-')])
+    return f'{d.year}{quarter_d[d.month]}'
+
+def get_quarterly_users_and_percentages(start_day, end_day, **kwargs):
+    """
+    returns the contents of the cloud_quarterly table with percentages
+    """
+    logging.info('Fetching quarterly usage percentages from database')
+    quarters = CloudQuarterly.objects.filter(
+        date__gte=start_day,
+        date__lte=end_day).order_by(
+        '-date')[:1]
+    quarter_d = {3: 'Q1', 6: 'Q2', 9: 'Q3', 12: 'Q4'}
+    result = dict()
+    result['cloud_users'] = []
+    result['total_projects_active'] = 0
+    fields = CloudQuarterly._meta.get_fields()
     for quarter in reversed(quarters):
-        fields = CloudQuarterly._meta.get_fields()
         # sum the faculty totals for a grand total
         total = 0
         for field in fields:
             if field.name is 'unknown' or field.name is 'date':
                 continue
             total += int(getattr(quarter, field.name))
-        print('%s' % total)
+        quarter_header = _get_quarter_abbreviation(getattr(quarter, 'date'))
         total_percentage = 0
-        # print out the percentage for each faculty
+        # add the percentage for each faculty
         for field in fields:
             if field.name is 'unknown' or field.name is 'date':
                 continue
             percentage = int(getattr(quarter, field.name)) / total * 100
-            print('%7s, %5s, %5s' % (
-                field.name, int(getattr(quarter, field.name)), 2),
-                  round(percentage, 2))
+            result['cloud_users'].append((
+                quarter_header,  # quarter abbreviation
+                field.name.upper(),  # faculty
+                int(getattr(quarter, field.name)),  # active users
+                round(percentage, 2)))  # percentage of UoM users
             total_percentage += percentage
-        # print the total percentage (should be 100, all going well)
-        print('%s' % round(total_percentage, 2))
+        # the total percentage should be 100, all going well
+        return result
 
 
-def build_quarterly_usage(extract_db, load_db, **kwargs):
+def get_quarterly_cpu_hours(start_day, end_day, **kwargs):
     """
-    Prints the quarterly usage in core hours.
+    Gets the quarterly usage in core hours.
     """
-    logging.info('Building quarterly core hours')
-    heading = 'Quarter'
-    faculties_list = Faculties.get_faculties_list()
-    for faculty in faculties_list:
-        heading += ', %s' % faculty
-    heading += ', Total'
-    print(heading)
-    for start_date, end_date in quarter_dates():
-        result_set = extract_db.get_core_hours(start_date, end_date)
-        totals = Faculties.get_new_totals()
-        percentage_totals = Faculties.get_new_totals()
-        for row in result_set:
-            project_id = row['project_id']
-            project_name = row['project_name']
-            try:
-                cpf = CloudProjectFaculty.objects.get(project_id=project_id)
-                faculty = cpf.allocated_faculty
-                totals[faculty] += row['vcpu_hours']
-            except CloudProjectFaculty.DoesNotExist:
-                logging.warning('%s with id %s does not exist' % (
-                    project_name, project_id))
-        grand_total = 0
-        for faculty, total in totals.items():
-            grand_total += total
-        total_percentage = 0
-        for faculty, total in totals.items():
-            percentage = (total * 100) / grand_total
-            percentage_totals[faculty] = percentage
-            total_percentage += percentage
-        line = '%s - %s' % (start_date, end_date)
-        print_totals = True # a hack to print totals or percentages
-        for faculty in faculties_list:
-            if print_totals:
-                line += ', %s' % totals[faculty]
-            else:
-                line += ', %.2f' % percentage_totals[faculty]
-        if print_totals:
-            line += ', %s' % grand_total
-        else:
-            line += ', %.2f' % total_percentage
-        print(line)
+    logging.info('Fetching quarterly core hours and percentages from database')
+    result = dict()
+    result['cloud_hours'] = []
+    # faculties_list = Faculties.get_faculties_list()
+    quarters = CloudQuarterlyCoreHours.objects.filter(
+        date__gte=start_day,
+        date__lte=end_day).order_by(
+        '-date')[:1]
+    fields = CloudQuarterlyCoreHours._meta.get_fields()
+    assert len(quarters) == 1
+    quarter = quarters.first()
+    # sum the faculty totals for a grand total
+    total = 0
+    for field in fields:
+        if field.name is 'unknown' or field.name is 'date':
+            continue
+        total += int(getattr(quarter, field.name))
+    # by working out the quarter abbreviation from the date value
+    # we can see that we are matching the date arguments
+    quarter_header = _get_quarter_abbreviation(getattr(quarter, 'date'))
+    total_percentage = 0
+    # add the percentage for each faculty
+    for field in fields:
+        if field.name is 'unknown' or field.name is 'date':
+            continue
+        percentage = int(getattr(quarter, field.name)) / total * 100
+        result['cloud_hours'].append((
+            quarter_header,  # quarter abbreviation
+            field.name.upper(),  # faculty
+            int(getattr(quarter, field.name)),  # active users
+            round(percentage, 2)))  # percentage of UoM users
+        total_percentage += percentage
+    # the total percentage should be 100, all going well
+    return result
+
+
+def get_quarterly_usage_figures(start_day, end_day, **kwargs):
+    """
+    :param start_day:
+    :param end_day:
+    :param kwargs:
+    :return: The usage figures for the quarter that falls between the start
+    day and the end day
+    """
+    logging.info('Fetching quarterly usage from database')
+    result = dict()
+    # faculties_list = Faculties.get_faculties_list()
+    quarters = CloudQuarterlyUsage.objects.filter(
+        date__gte=start_day,
+        date__lte=end_day).order_by(
+        '-date')[:1]
+    assert len(quarters) == 1
+    quarter = quarters.first()
+    result['total_projects_active'] = int(
+        getattr(quarter, 'projects_active'))
+    result['uom_projects_active'] = int(
+        getattr(quarter, 'uom_projects_active'))
+    result['uom_users_active'] = int(getattr(quarter, 'uom_users_active'))
+    return result
