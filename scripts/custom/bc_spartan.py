5e05be3caa9ed412a8f0b20266cacb18b9b59870
Martin Paulo <martin.paulo@gmail.com>
2019-01-24 15:00:08 +1100

    Added some further notes.

--- scripts/custom/bc_spartan.py
+++ scripts/custom/bc_spartan.py
@@ -8,21 +8,23 @@ from django.template.loader import get_template
 from reports_beta.local_settings import SPARTAN_MDHS_USERS
 from scripts.cloud.utility import Faculties, quarter_dates, LDAP
 
+
 # ssh into spartan: ssh -i ~/.ssh/mp_unimelb2 root@spartan-m.hpc.unimelb.edu.au
 # Run the following sreport command with the desired date range:
 # This produces a report giving Account|Login|Used, where used is in minutes
 # sreport -P cluster AccountUtilizationByUser start=2017-01-01 end=2017-03-31 format=Accounts,Login,Used
 
+
 # following prints job counts by cpu sizes, with a percentage of the cluster consumed
 # sreport job SizesByAccount PrintJobCount cluster=spartan start=2016-10-01 end=2016-12-31
 
-# To get the job count for the quarter, run the following and then sum the count of jobs to produce the grand total
+# To get the job count for the quarter, run the following and then sum the
+# count of jobs to produce the grand total
 # sreport job SizesByAccount PrintJobCount grouping=1 cluster=spartan start=2016-07-01 end=2016-09-30
 
-# To get the core compute hours, run the following, then take the 'Reported' total
+# To get the total core compute hours, run the following, then take the
+# 'Reported' total
 # sreport cluster Utilization cluster=spartan start=2016-07-01 end=2016-09-30 -t Hours
-
-# For more on sreport see: https://slurm.schedmd.com/sreport.html
 
 # to get the users:
 # ssh -i ~/.ssh/mp_unimelb2 root@dashboard.hpc.unimelb.edu.au
@@ -40,13 +42,18 @@ from scripts.cloud.utility import Faculties, quarter_dates, LDAP
 # Then use the location to place your output file:
 
 # SELECT username, email, full_name, department
-# INTO OUTFILE '/var/lib/mysql-files/2017_q3_spartan_users.csv'
+# INTO OUTFILE '/tmp/2018_q4_spartan_users.csv'
 # FIELDS TERMINATED BY ','
 # ENCLOSED BY '"'
 # LINES TERMINATED BY '\n'
 # FROM person
 # WHERE institute_id = 1
 # ORDER BY username;
+
+# links of interest:
+# https://ubccr.freshdesk.com/support/solutions/articles/5000686909-how-to-retrieve-job-history-and-accounting
+# https://wiki.fysik.dtu.dk/niflheim/Slurm_accounting
+# https://slurm.schedmd.com/sreport.html
 
 quarters = []
 file_names = []
@@ -101,9 +108,9 @@ def total_project_job_counts(input_file_name, allocated_projects):
     with input_path.open() as input_file:
         lines = input_file.readlines()
         for line in lines:
+            # skip comment lines
             if line.startswith('#'):
                 continue
-            # print(line)
             split = line.split()
             project_name = split[1]
             if project_name in allocated_projects:
@@ -112,14 +119,16 @@ def total_project_job_counts(input_file_name, allocated_projects):
     return total_job_count
 
 
-def get_hours_and_minutes(sum_minutes):
-    # the sreport output is in minutes. To move to hours:minutes
+def get_hours_and_minutes_str(sum_minutes):
+    # the sreport output can sometimes be in minutes. If so, this converts to
+    # a string showing the hour representation. The rounding is down.
+    # If more accurate minutes are require change to:
     # return f'{sum_minutes//60}:{sum_minutes % 60}'
-    # and to just round down:
     return f'{sum_minutes//60}'
 
 
 if __name__ == "__main__":
+
     import django
     from scripts.config import Configuration
 
@@ -202,9 +211,9 @@ if __name__ == "__main__":
                 for faculty, total in faculty_totals.items():
                     grand_total += total
                     hpc_core_hours['hpc_hours'].append(
-                        (current_quarter, faculty.value, get_hours_and_minutes(total)))
+                        (current_quarter, faculty.value, get_hours_and_minutes_str(total)))
                 hpc_core_hours['hpc_total_core_hours'] = str(
-                    get_hours_and_minutes(grand_total))
+                    get_hours_and_minutes_str(grand_total))
                 hpc_core_hours['quarter'] = current_quarter
                 grand_total = 0
                 for faculty, project_count in faculty_project_count.items():
